1afe61314fef54ebfca15dd28f1df21b95a50725
Juan José Jiménez Monsalve <jjsanchez689@gmail.com>
2024-07-26 18:44:48 -0500

    feat: add component atom custom text field

---
+++ lib/src/components/atoms/custom_text_field.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String? labelText;
+  final String? hintText;
+  final TextEditingController? controller;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+  final Icon? prefixIcon;
+  final String? Function(String?)? validator;
+
+  const CustomTextField({
+    super.key,
+    this.labelText,
+    this.hintText,
+    this.controller,
+    this.keyboardType,
+    this.obscureText = false,
+    this.prefixIcon,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText,
+        prefixIcon: prefixIcon,
+        border: const OutlineInputBorder(),
+      ),
+      validator: validator,
+    );
+  }
+}
