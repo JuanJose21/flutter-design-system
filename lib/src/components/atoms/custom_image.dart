d1514d4c252fde2430faa47b9c2de9313a09f1f0
Juan José Jiménez Monsalve <jjsanchez689@gmail.com>
2024-08-01 10:08:56 -0500

    feat: add custom image atom widget

---
+++ lib/src/components/atoms/custom_image.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class CustomImage extends StatelessWidget {
+  final String imageUrl;
+  final double width;
+  final double height;
+  final double borderRadius;
+  final BoxFit fit;
+  final Color placeholderColor;
+
+  const CustomImage({
+    super.key,
+    required this.imageUrl,
+    this.width = 100.0,
+    this.height = 100.0,
+    this.borderRadius = 8.0,
+    this.fit = BoxFit.cover,
+    this.placeholderColor = Colors.grey,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(borderRadius),
+      child: Image.network(
+        imageUrl,
+        width: width,
+        height: height,
+        fit: fit,
+        loadingBuilder: (BuildContext context, Widget child,
+            ImageChunkEvent? loadingProgress) {
+          if (loadingProgress == null) {
+            return child;
+          } else {
+            return Container(
+              width: width,
+              height: height,
+              color: placeholderColor,
+              child: Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                          (loadingProgress.expectedTotalBytes ?? 1)
+                      : null,
+                ),
+              ),
+            );
+          }
+        },
+        errorBuilder:
+            (BuildContext context, Object error, StackTrace? stackTrace) {
+          return Container(
+            width: width,
+            height: height,
+            color: placeholderColor,
+            child: const Icon(Icons.broken_image, color: Colors.white),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/src/components/atoms/index.dart
+++ lib/src/components/atoms/index.dart
@@ -2,3 +2,4 @@ export 'custom_button.dart';
 export 'custom_icon.dart';
 export 'custom_label.dart';
 export 'custom_text_field.dart';
+export 'custom_image.dart';
